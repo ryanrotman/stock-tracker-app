0b4d18f247c6cc7028b55a5ae83a573797f01d3c
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-09 01:32:29 -0600

    test login card and added to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,10 @@
+import LoginCard from "../components/LoginCard";
+
 function Login() {
     return (
         <div className="container">
             <h3>THIS IS THE LOG IN PAGE</h3>
+            <LoginCard />
         </div>
     )
 }
