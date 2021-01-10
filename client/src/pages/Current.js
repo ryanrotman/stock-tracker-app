ac989b784aa0e0e2190b661ac37ac04952f44754
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-10 14:35:24 -0600

    added back in some page subtitles

--- client/src/pages/Current.js
+++ client/src/pages/Current.js
@@ -4,6 +4,7 @@ import SearchFeature from "../components/SearchFeature";
 function Current() {
     return (
         <div className="container">
+            <h3 className="center-align">Stocks Currently Invested In</h3>
             <br />
             <div className="row">
                 <SearchFeature />
--- client/src/pages/Interested.js
+++ client/src/pages/Interested.js
@@ -3,6 +3,7 @@ import SearchFeature from "../components/SearchFeature";
 function Interested() {
     return (
         <div className="container">
+            <h3 className="center-align">Stocks Interested In</h3>
             <br />
             <div className="row">
                 <SearchFeature />
