a6abf2150b5bc575ff87bb8a805dc75360801625
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-10 23:58:32 -0600

    edited the various API needs in the utils folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,21 +3,25 @@ import axios from "axios";
 const APIkey = process.env.REACT_APP_ALPHAVANTAGE_API_KEY
 
 const API = {
-    // Gets all books
+    // Gets stock names/symbols
     getStockNames: (query) => {
         return axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${APIkey}`);
     },
-    // Gets the book with the given id
-    getBook: function (id) {
-        return axios.get("/api/books/" + id);
+    // Gets stock data for graph
+    getStockData: (query) => {
+        return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${query}&outputsize=compact&apikey=${API_KEY}`);
     },
-    // Deletes the book with the given id
-    deleteBook: function (id) {
-        return axios.delete("/api/books/" + id);
+    // Gets the stock with the given id
+    getStock: function (id) {
+        return axios.get("/api/stocks/" + id);
     },
-    // Saves a book to the database
-    saveBook: function (bookData) {
-        return axios.post("/api/books", bookData);
+    // Deletes the stock with the given id
+    deleteStock: function (id) {
+        return axios.delete("/api/stocks/" + id);
+    },
+    // Saves a stock to the database
+    saveStock: function (stockData) {
+        return axios.post("/api/stocks", stockData);
     }
 };
 
