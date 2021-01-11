781c678c81a0b506825e26e2e8886b6d3843982d
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-11 00:30:40 -0600

    updated wording to better describe what code is doing

--- models/stock.js
+++ models/stock.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const stockSchema = new Schema({
+const stockSymbolSchema = new Schema({
     // TODO: fix the below schema
     user: { type: String, required: true },
     symbol: { type: String, required: true },
     status: { type: String, required: true }
 });
 
-const Stock = mongoose.model("Stock", stockSchema);
+const StockSymbol = mongoose.model("Stock", stockSymbolSchema);
 
-module.exports = Stock;
+module.exports = StockSymbol;
