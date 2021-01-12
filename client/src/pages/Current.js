67dbefce458a55ecb749853cdeae32bdec18da93
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-12 16:53:47 -0600

    moved code around and finished building out functionality that successfully searches for a stock, saves that stock to the DB, and builds the graph section with tabs and graph section that still needs worked on

--- client/src/pages/Current.js
+++ client/src/pages/Current.js
@@ -1,29 +1,93 @@
 import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Stock from "../components/Stock";
-import SearchFeature from "../components/SearchFeature";
+import SearchBox from "../components/SearchBox";
+import SearchResults from "../components/SearchResults";
 import API from "../utils/API";
+import StockGraphs from "../components/StockGraphs";
+import M from "materialize-css";
 
 function Current() {
 
     const [stocks, setStocks] = useState([])
+    const [stockNames, setStockNames] = useState([]);
+    const [formInput, setFormInput] = useState({
+        search: ""
+    });
 
     const { user } = useAuth0();
 
     useEffect(() => {
+        console.log("LOADING STOCKS FROM DATABASE");
         loadStocks();
     }, []);
+
+    function tabInit() {
+            let el = document.querySelectorAll('.tabs');
+            // TODO: to avoid the overuse of the API call at the beginning of tabs being built, use the onShow function option in the init
+            // TODO: adjust the below onTabShow function to have that be the API call to get the stock data to build the graph
+            M.Tabs.init(el, { onShow: onTabShow });
+    }
+
+    function onTabShow() {
+        console.log("STOCK GRAPH CONTENT SHOULD LOAD NOW");
+    }
+
+    function handleInputChange(event) {
+        const { value } = event.target;
+        setFormInput({
+            search: value
+        })
+    }
+
+    console.log("FORM INPUT----------> ", formInput);
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formInput.search) {
+            API.getStockNames(formInput.search).then((res) => {
+                // console.log("API RES----------> ", res);
+                // console.log("API RES.DATA----------> ", res.data);
+                console.log("API RES.DATA.BESTMATCHES----------> ", res.data.bestMatches);
+                setStockNames(res.data.bestMatches);
+            })
+            .then(() => setFormInput({
+                search: ""
+            })).catch(err => console.log(err));
+        };
+    };
+
+    function handleStockSelection(event) {
+        event.preventDefault();
+        // console.log("StockSelection----------> ", event);
+        // console.log("Window: ", window);
+        let status = window.location.pathname;
+        status = status.slice(1)
+        let stockSymbol = event.target.dataset.value;
+        console.log("StockSelection: ", `Stock ${stockSymbol} has been selected for the ${status} page.`);
+        console.log("LOGGED IN USER: ", user.sub);
+
+        API.saveStock({
+            user: user.sub,
+            symbol: stockSymbol,
+            status: status
+        })
+
+        loadStocks();
+        setStockNames([]);
+
+        M.toast({html: `Stock ${stockSymbol} has been saved!`})
+    }
 
     function loadStocks() {
         const currentUser = user.sub;
 
-        // FIXME: api call is not returning the stated user and status, it's returning the whole database
         API.getStock({
             user: currentUser,
             status: "current"
         }).then((res) => {
             console.log("LOAD CURRENT STOCKS RES: ", res);
             setStocks(res.data);
+            tabInit();
         }).catch((err) => console.log(err));
     };
 
@@ -32,16 +96,25 @@ function Current() {
             <h3 className="center-align">Stocks Currently Invested In</h3>
             <br />
             <div className="row">
-                <SearchFeature />
+                <div className="col s12 m6 card">
+                    <SearchBox
+                        onChange={handleInputChange}
+                        name="search"
+                        onClick={handleFormSubmit}
+                        value={formInput.search}
+                    />
+                </div>
+                <div className="col s12 m6">
+                    <SearchResults
+                        stockNames={stockNames}
+                        onClick={handleStockSelection}
+                    />
+                </div>
             </div>
-            {/* <p>This is an example of the stock graph brought in:</p> */}
-            {/* <Stock /> */}
-            <h6>Saved Stocks - Current:</h6>
-            <ul>
-                {stocks.map(stock => (
-                    <li><strong>User:</strong> {stock.user} <br /> <strong>Symbol:</strong> {stock.symbol} <br /> <strong>Status:</strong> {stock.status} <br /> ----------</li>
-                ))}
-            </ul>
+            <h6>Stock Graph Section:</h6>
+            <StockGraphs
+                stocks={stocks}
+            />
         </div>
     )
 }
