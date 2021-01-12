0421268e2471b6482559d3a4af62781cc6f52936
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-12 16:52:18 -0600

    added console log to findAll

--- controllers/stocksController.js
+++ controllers/stocksController.js
@@ -3,6 +3,7 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
     findAll: function (req, res) {
+        console.log(req.query);
         db.Stock
             .find(req.query)
             .sort({ date: -1 })
