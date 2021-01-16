a808cde6e3a40fb2d3ca8c68b3b0d0babe8bb0ff
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-16 12:55:53 -0600

    #12 built out components and added to the dashboard page to finish page that user sees upon login

---
+++ client/src/components/InterestedChoice/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./index.css"
+
+function InterestedChoice() {
+    return (
+        <div className="col s12 m6">
+            <h4 className="header center-align">Interested In Stocks:</h4>
+            <div className="card horizontal">
+            {/* <div className="card-image">
+                <i className="fas fa-search"></i>
+            </div> */}
+            <div className="card-stacked">
+                <div className="card-action center-align">
+                <Link to="/interested" className="link-interested">Click here to view interested in stocks</Link>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default InterestedChoice;
