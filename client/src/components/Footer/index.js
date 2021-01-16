e66ae2ab64d908500d84b40342c502cf5d3171f0
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-16 14:25:12 -0600

    #17 built footer component and styling

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="page-footer black">
+            <div className="footer-copyright">
+                <div className="container center-align">
+                    Copyright © 2021 Stock Tracker
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
