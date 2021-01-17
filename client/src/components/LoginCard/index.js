a2c8b456115dcb1bc9b161b38895fe8589296946
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-17 00:47:45 -0600

    added background image and styling of login card to login page

--- client/src/components/LoginCard/index.js
+++ client/src/components/LoginCard/index.js
@@ -6,7 +6,7 @@ function LoginCard() {
         // TODO: might need to add back commented out row and col lines for proper mobile responsiveness
         <div className="row">
             <div className="col s6 offset-s3 valign">
-            <div className="card-panel black center-align">
+            <div className="card-panel black center-align" style={{ boxShadow: "0 4px 8px 0 rgba(245, 245, 245, 0.2), 0 6px 20px 0 rgba(245, 245, 245, 0.19)" }}>
                 <span className="white-text">
                     Welcome to your stock investment tracker!
                     {/* <p>Here you can keep track of stocks you are currently invested in as well as any that you are interested in.</p> */}
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,12 @@
 import LoginCard from "../components/LoginCard";
+import backgroundImg from "../assets/images/Stock-Trading.png"
 
 function Login() {
     return (
-        <div className="container login-container valign-wrapper">
-            <LoginCard />
+        <div style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: "no-repeat", height: "100%", backgroundPosition: "center", backgroundSize: "cover" }}>
+            <div className="container login-container valign-wrapper">
+                <LoginCard />
+            </div>
         </div>
     )
 }
