6d19752fb1eb5809e7f7640693e95d5dd20fb769
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-19 13:39:33 -0600

    added check for isAuthenticated to change what links display in the navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,8 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import M from "materialize-css";
-import LoginBtn from "../LoginBtn"
-import LogoutBtn from "../LogoutBtn";
 import AuthenticationBtn from "../AuthenticationBtn";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Nav() {
 
@@ -11,6 +10,36 @@ function Nav() {
         let sidenav = document.querySelector('#nav-mobile');
         M.Sidenav.init(sidenav, {});
     }, []);
+
+    const { isAuthenticated } = useAuth0();
+
+    if (isAuthenticated) {
+        return (
+            <div>
+                <div className="navbar-fixed">
+                    <nav className="black" role="navigation">
+                        <div className="nav-wrapper container">
+                        <a id="logo-container" className="brand-logo">Stock Tracker</a>
+                        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><Link to="/dashboard">Dashboard</Link></li>
+                                <li><Link to="/current">Current Stocks</Link></li>
+                                <li><Link to="/interested">Interested Stocks</Link></li>
+                                <li><AuthenticationBtn /></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+    
+                <ul id="nav-mobile" className="right sidenav">
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/current">Current Stocks</Link></li>
+                    <li><Link to="/interested">Interested Stocks</Link></li>
+                    <li><AuthenticationBtn /></li>
+                </ul>
+            </div>
+        );
+    }
 
     return (
         <div>
@@ -20,10 +49,6 @@ function Nav() {
                     <a id="logo-container" className="brand-logo">Stock Tracker</a>
                     <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                         <ul className="right hide-on-med-and-down">
-                            {/* <li><Link to="/">Home</Link></li> */}
-                            <li><Link to="/dashboard">Dashboard</Link></li>
-                            <li><Link to="/current">Current Stocks</Link></li>
-                            <li><Link to="/interested">Interested Stocks</Link></li>
                             <li><AuthenticationBtn /></li>
                         </ul>
                     </div>
@@ -31,14 +56,11 @@ function Nav() {
             </div>
 
             <ul id="nav-mobile" className="right sidenav">
-                {/* <li className="white-text"><Link to="/">Home</Link></li> */}
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/current">Current Stocks</Link></li>
-                <li><Link to="/interested">Interested Stocks</Link></li>
                 <li><AuthenticationBtn /></li>
             </ul>
         </div>
     );
+
 }
 
 export default Nav;
