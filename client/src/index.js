47fe72f1d4272ad26ea31348b1a49d17f4860fca
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-04 16:13:37 -0600

    code cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -15,21 +15,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// ----- THIS SECTION SUPPOSEDLY NOT NEEDED WITH USING AUTH0PROVIDERWITHHISTORY -----
-
-// import { Auth0Provider } from "@auth0/auth0-react";
-
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-// ReactDOM.render(
-//     <Auth0Provider
-//         domain={domain}
-//         clientId={clientId}
-//         redirectUri="http://localhost:3000/dashboard"
-//     >
-//         <App />
-//     </Auth0Provider>,
-//     document.getElementById("root")
-// );
