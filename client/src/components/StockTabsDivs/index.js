a3df962c8447f9373e6cb82472806bcb3da0a7ee
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-08 13:54:57 -0600

    added row col for better mobile responsiveness

--- client/src/components/StockTabsDivs/index.js
+++ client/src/components/StockTabsDivs/index.js
@@ -25,14 +25,32 @@ function StockTabsDivs(props) {
 
     return (
         <div id={props.symbol} className="col s12">
-            <h5>{props.company} &#40;{props.symbol}&#41;
+            <div className="row">
+                <div className="col s12 m6">
+                    <h5>{props.company} &#40;{props.symbol}&#41;</h5>
+                </div>
+                <div className="col s12 m6">
+                    <button style={{float:"right", marginTop:"12px"}} className="waves-effect waves-light btn stock-btn" onClick={() => props.onClick(props.id)}>
+                        Delete Stock
+                    </button>
+                    <button style={{float:"right", marginTop:"12px"}} className="waves-effect waves-light btn stock-btn" value={props.newStatus} id={props.id} onClick={props.onUpdate}>
+                        Move to {props.newStatus}
+                    </button>
+                </div>
+                {/* <div className="col s12 m3">
+                    <button style={{float:"right"}} className="waves-effect waves-light btn stock-btn" value={props.newStatus} id={props.id} onClick={props.onUpdate}>
+                        Move to {props.newStatus}
+                    </button>
+                </div> */}
+            </div>
+            {/* <h5>{props.company} &#40;{props.symbol}&#41;
                 <button style={{float:"right"}} className="waves-effect waves-light btn stock-btn" onClick={() => props.onClick(props.id)}>
                     Delete Stock
                 </button>
                 <button style={{float:"right"}} className="waves-effect waves-light btn stock-btn" value={props.newStatus} id={props.id} onClick={props.onUpdate}>
                     Move to {props.newStatus}
                 </button>
-            </h5>
+            </h5> */}
             <StockGraphs
                 xValues={props.xValues}
                 yValues={props.yValues}
