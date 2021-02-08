0e31dd1988d28bb3ac1fc5d1116c8a2b43e150b6
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-08 14:46:39 -0600

    edited layout and code to show latest posted stock data

--- client/src/components/StockTabsDivs/index.js
+++ client/src/components/StockTabsDivs/index.js
@@ -5,17 +5,28 @@ import "./index.css";
 function StockTabsDivs(props) {
 
     let today = moment().format("YYYY-MM-DD");
-    let longDate = moment().format("dddd, MMMM DD, YYYY")
+    // let longDate = moment().format("dddd, MMMM DD, YYYY")
     console.log("TODAY IS: ", today);
 
     // console.log("STOCK DATA PASSED INTO TABS DIVS", props.stockData);
     // console.log("GRAB THE TIME SERIES SECTION: ", props.stockData["Time Series (Daily)"]);
     // console.log("GRAB THE CURRENT DAY: ", props.stockData["Time Series (Daily)"]["2021-01-15"]);
 
+    // let stockDataObj = props.stockData["Time Series (Daily)"];
+    // let keys = Object.keys(stockDataObj);
+    // console.log("STOCK DATA KEYS:", keys);
+    // console.log("THE LAST DAY OF DATA DATE: ", keys[0])
+    // let longDate = moment(keys[0]).format("dddd, MMMM DD, YYYY");
+    // console.log("THE LAST DAY OF DATA: ", props.stockData["Time Series (Daily)"][keys[0]]);
+
     let todaysData;
+    let longDate;
     if (props.stockData["Time Series (Daily)"]) {
-        todaysData = props.stockData["Time Series (Daily)"][today];
-    };
+        let stockDataObj = props.stockData["Time Series (Daily)"];
+        let keys = Object.keys(stockDataObj);
+        todaysData = props.stockData["Time Series (Daily)"][keys[0]];
+        longDate = moment(keys[0]).format("dddd, MMMM DD, YYYY");
+    } ;
 
     // let dataObject = props.stockData["Time Series (Daily)"];
 
@@ -42,8 +53,9 @@ function StockTabsDivs(props) {
                 xValues={props.xValues}
                 yValues={props.yValues}
             />
-            {todaysData === undefined ? <h4 className="center-align"><strong>Market is closed today, {longDate}.</strong></h4> : 
-            <ul className="center-align">
+            {todaysData === undefined ? <h5 className="center-align"><strong>No stock data to report.</strong></h5> : 
+            <ul className="center-align stock-data-list">
+                <li><strong>Stock Data from: {longDate}</strong></li>
                 <li><strong>Open:</strong> {todaysData["1. open"]}</li>
                 <li><strong>High:</strong> {todaysData["2. high"]}</li>
                 <li><strong>Low:</strong> {todaysData["3. low"]}</li>
