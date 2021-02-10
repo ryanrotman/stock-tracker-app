ac700ac6228aa117588669ad13ec681efbbaa5fd
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-09 23:23:03 -0600

    code cleanup

--- client/src/components/CurrentChoice/index.js
+++ client/src/components/CurrentChoice/index.js
@@ -6,9 +6,6 @@ function CurrentChoice() {
         <div className="col s12 m6">
             <h4 className="header center-align">Current Stocks:</h4>
             <div className="card horizontal">
-            {/* <div className="card-image">
-                <i className="fas fa-search"></i>
-            </div> */}
             <div className="card-stacked">
                 <div className="card-action center-align">
                 <Link to="/current" className="link-current">Click here to view current stocks</Link>
