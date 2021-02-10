6bff0142f121e3533e82dd9f767554ca81e4e0a5
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-10 13:31:13 -0600

    code cleanup

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,8 +3,6 @@ import Profile from "../components/Profile";
 function Dashboard() {
     return (
         <div className="container">
-            {/* <br />
-            <h5>Welcome back! Keep track of your current stocks and stocks interested in by visiting the links below! Happy investing!</h5> */}
             <br />
             <Profile />
         </div>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,28 +3,28 @@ import axios from "axios";
 const APIkey = process.env.REACT_APP_ALPHAVANTAGE_API_KEY
 
 const API = {
-    // Gets stock names/symbols
+    // Get stock names/symbols
     getStockNames: (query) => {
         return axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${APIkey}`);
     },
-    // Gets stock data for graph
+    // Get stock data for graph
     getStockData: (query) => {
         return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${query}&apikey=${APIkey}`);
     },
-    // Gets the stock with the given id
+    // Get stock with the given id
     getStock: function (stockData) {
         console.log("STOCK DATA: ", stockData);
         return axios.get("/api/stocks/", {params: stockData});
     },
-    // Deletes the stock with the given id
+    // Delete stock with the given id
     deleteStock: function (id) {
         return axios.delete("/api/stocks/" + id);
     },
-    // Saves a stock to the database
+    // Save stock to the database
     saveStock: function (stockData) {
         return axios.post("/api/stocks", stockData);
     },
-    // Updates a stock in the database with the given id
+    // Update stock in the database with the given id
     updateStock: function (id, {status}) {
         return axios.put("/api/stocks/" + id, {status});
     }
