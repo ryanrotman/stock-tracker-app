b999c402cc741f41b86ccbd76a54fd44852a6b04
Ryan Rotman <ryan.rotman@gmail.com>
2021-02-10 13:17:11 -0600

    code cleanup

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -3,7 +3,6 @@ import SearchResultsBtn from "../SearchResultsBtn";
 function SearchResults(props) {
     return (
         <div className="col s12">
-            {/* <h2 className="header">Horizontal Card</h2> */}
             <div className="card horizontal">
                 <div className="card-stacked">
                     <div className="card-content">
